9552fd518def99a41265a233f11a493790b694be
Artem Dekhanov <zohanaizer2@gmail.com>
2019-05-31 22:45:54 +0300

    Updates

---
+++ precache-manifest.b0fff897f8e16d53c06566c14678f211.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "a6f1c2cdae0f9845b0e9",
+    "url": "/react-weather/static/css/main.d14343cd.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d31623a89ef5af501573",
+    "url": "/react-weather/static/js/1.d31623a8.chunk.js"
+  },
+  {
+    "revision": "8ad3cef1c2506df250b9",
+    "url": "/react-weather/static/js/runtime~main.8ad3cef1.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "a6f1c2cdae0f9845b0e9",
+    "url": "/react-weather/static/js/main.a6f1c2cd.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "d31623a89ef5af501573",
+    "url": "/react-weather/static/css/1.9d1f2e12.chunk.css"
+  },
+  {
+    "revision": "bdf5fadc5285d2dbfe9cb4f1741ee0ca",
+    "url": "/react-weather/index.html"
+  }
+];
